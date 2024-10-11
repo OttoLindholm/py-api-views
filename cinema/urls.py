207b8db0bc20fc887df2d0467eed1ac4519a9822
Sergiy Gorbachov <ottolindholmplay@gmail.com>
2024-10-12 00:57:16 +0300

    Add routes for MovieViewSet view, change CinemaHallViewSet base class to correct

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
-from cinema.views import GenreList, GenreDetail, ActorList, ActorDetail, CinemaHallViewSet
+from cinema.views import GenreList, GenreDetail, ActorList, ActorDetail, CinemaHallViewSet, MovieViewSet
 
 cinema_hall_list = CinemaHallViewSet.as_view(actions={
     "get": "list",
@@ -8,11 +9,14 @@ cinema_hall_list = CinemaHallViewSet.as_view(actions={
 })
 
 cinema_hall_detail = CinemaHallViewSet.as_view(actions={
-    "get": "retrive",
+    "get": "retrieve",
     "put": "update",
     "patch": "partial_update",
     "delete": "destroy",
 })
+
+router = routers.DefaultRouter()
+router.register("movies", MovieViewSet)
 
 urlpatterns = [
     path("genres/", GenreList.as_view(), name="genres-list"),
@@ -21,6 +25,7 @@ urlpatterns = [
     path("actors/<int:pk>", ActorDetail.as_view(), name="actor-detail"),
     path("cinema-halls/", cinema_hall_list, name="cinema-hall-list"),
     path("cinema-halls/<int:pk>/", cinema_hall_detail, name="cinema-hall-detail"),
+    path("", include(router.urls)),
 ]
 
 app_name = "cinema"
